206d85b76d40135ff1daea304daea4675e9bc8c3
zfhrp <zfhrp6@users.noreply.github.com>
2020-10-24 07:36:43 +0900

    ignore asset hash in snapshot

--- test/mem2cw.test.ts
+++ test/mem2cw.test.ts
@@ -2,6 +2,16 @@ import { SynthUtils } from '@aws-cdk/assert';
 import * as cdk from '@aws-cdk/core';
 import * as Mem2Cw from '../lib/mem2cw-stack';
 
+expect.addSnapshotSerializer({
+  test: (val) => typeof val === 'string',
+  print: (val) => {
+    const v = <string>val;
+    const newVal = v.replace(/AssetParameters([A-Fa-f0-9]{64})(\w+)/, '[HASH REMOVED]');
+    const newVal2 = newVal.replace(/(\w+) (\w+) for asset\s?(version)?\s?"([A-Fa-f0-9]{64})"/, '[HASH REMOVED]');
+    return `"${newVal2}"`;
+  },
+});
+
 test('Snapshot Test', () => {
   const app = new cdk.App();
   // WHEN
