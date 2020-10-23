5eff93a0a58dbf9ed0f77a0a09fe586bf948d0b9
zfhrp <zfhrp6@users.noreply.github.com>
2020-10-24 04:54:33 +0900

    fix test

--- test/mem2cw.test.ts
+++ test/mem2cw.test.ts
@@ -1,13 +1,11 @@
-import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
+import { SynthUtils } from '@aws-cdk/assert';
 import * as cdk from '@aws-cdk/core';
 import * as Mem2Cw from '../lib/mem2cw-stack';
 
-test('Empty Stack', () => {
-    const app = new cdk.App();
-    // WHEN
-    const stack = new Mem2Cw.Mem2CwStack(app, 'MyTestStack');
-    // THEN
-    expectCDK(stack).to(matchTemplate({
-      "Resources": {}
-    }, MatchStyle.EXACT))
+test('Snapshot Test', () => {
+  const app = new cdk.App();
+  // WHEN
+  const stack = new Mem2Cw.Mem2CwStack(app, 'MyTestStack');
+  // THEN
+  expect(SynthUtils.toCloudFormation(stack)).toMatchSnapshot();
 });
